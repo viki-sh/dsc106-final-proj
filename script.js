d4e88051d8933c07473d945634d501ec56e4aefa
Viki Shi <vishi@ucsd.edu>
2025-05-31 15:42:43 -0700

    init

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const margin = { top: 50, right: 200, bottom: 100, left: 50 };
 const width = 3000;
-const height = 400;
+const height = 300;
 
 let vitalsData, interventionsData;
 let currentCase = "1";
@@ -9,97 +9,111 @@ let speed = 1;
 let interval;
 let currentIndex = 0;
 
-const svg = d3
-  .select("#chart")
-  .append("svg")
+const svgVitals = d3.select("#chart")
   .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom);
-
-const g = svg
+  .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
 
-let x = d3.scaleTime().range([0, width]);
-const y = d3.scaleLinear().range([height, 0]);
+const svgInterventions = d3.select("#intervention-chart")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left},${margin.top})`);
+
+const x = d3.scaleTime().range([0, width]);
+const yVitals = d3.scaleLinear().range([height, 0]);
+const yInterventions = d3.scaleLinear().range([height, 0]);
+
 const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-const line = d3
-  .line()
+const lineVitals = d3.line()
   .x(d => x(new Date(d.time)))
-  .y(d => y(d.value));
+  .y(d => yVitals(d.value));
 
-d3.json("vital_data.json").then(data => {
-  vitalsData = data;
-  init();
-});
+const lineInterventions = d3.line()
+  .x(d => x(new Date(d.time)))
+  .y(d => yInterventions(d.value));
 
-d3.json("proxy_drug_data.json").then(data => {
-  interventionsData = data;
+d3.json("vital_data.json").then(vData => {
+  vitalsData = vData;
+  d3.json("proxy_drug_data.json").then(iData => {
+    interventionsData = iData;
+    init();
+  });
 });
 
 function init() {
-  const caseData = vitalsData[currentCase];
-  const vitals = Object.keys(caseData);
-  const allValues = vitals.flatMap(key => caseData[key].map(d => d.value));
-  const allTimes = vitals.flatMap(key => caseData[key].map(d => new Date(d.time)));
+  const caseVitals = vitalsData[currentCase];
+  const caseInterventions = interventionsData[currentCase];
+
+  const allVitals = Object.keys(caseVitals);
+  const allIntervs = Object.keys(caseInterventions);
+
+  const allVitalTimes = allVitals.flatMap(k => caseVitals[k].map(d => new Date(d.time)));
+  const allIntervTimes = allIntervs.flatMap(k => caseInterventions[k].map(d => new Date(d.time)));
+  const allTimes = allVitalTimes.concat(allIntervTimes);
+
+  const allVitalValues = allVitals.flatMap(k => caseVitals[k].map(d => d.value));
+  const allIntervValues = allIntervs.flatMap(k => caseInterventions[k].map(d => d.value));
 
   const minTime = d3.min(allTimes);
-  const maxTime = new Date(minTime.getTime() + 20 * 60 * 1000); // stretch to 20 minutes
-  x.domain([minTime, maxTime]);
-  y.domain([0, d3.max(allValues)]);
-  color.domain(vitals);
+  const maxTime = new Date(minTime.getTime() + 20 * 60 * 1000);  // 20 mins stretched
 
-  vitals.forEach(key => {
-    g.append("path")
-      .datum(caseData[key])
+  x.domain([minTime, maxTime]);
+  yVitals.domain([0, d3.max(allVitalValues)]);
+  yInterventions.domain([0, d3.max(allIntervValues)]);
+
+  color.domain([...allVitals, ...allIntervs]);
+
+  allVitals.forEach(key => {
+    svgVitals.append("path")
+      .datum(caseVitals[key])
       .attr("fill", "none")
       .attr("stroke", color(key))
       .attr("stroke-width", 1.5)
-      .attr("d", line)
+      .attr("d", lineVitals)
       .attr("class", `line ${key}`);
   });
 
-  g.append("g")
+  allIntervs.forEach(key => {
+    svgInterventions.append("path")
+      .datum(caseInterventions[key])
+      .attr("fill", "none")
+      .attr("stroke", color(key))
+      .attr("stroke-width", 1.5)
+      .attr("d", lineInterventions)
+      .attr("class", `line ${key}`);
+  });
+
+  svgVitals.append("g")
     .attr("transform", `translate(0,${height})`)
     .call(d3.axisBottom(x));
 
-  g.append("g").call(d3.axisLeft(y));
+  svgVitals.append("g").call(d3.axisLeft(yVitals));
 
-  const legend = svg
-    .append("g")
-    .attr("transform", `translate(${width + margin.left + 20}, ${margin.top})`);
+  svgInterventions.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x));
 
-  vitals.forEach((key, i) => {
-    legend.append("rect")
-      .attr("x", 0)
-      .attr("y", i * 20)
-      .attr("width", 10)
-      .attr("height", 10)
-      .attr("fill", color(key));
-
-    legend.append("text")
-      .attr("x", 20)
-      .attr("y", i * 20 + 9)
-      .text(key);
-  });
-
-  updateSidebar();
+  svgInterventions.append("g").call(d3.axisLeft(yInterventions));
 }
 
-function updateSidebar() {
-  const vitals = Object.keys(vitalsData[currentCase]);
-  const container = d3.select("#vital-values").html("");
+function updateChart() {
+  const caseVitals = vitalsData[currentCase];
+  const caseInterventions = interventionsData[currentCase];
 
-  vitals.forEach(key => {
-    container
-      .append("div")
-      .attr("class", "vital-value")
-      .attr("id", `vital-${key}`)
-      .text(`${key}: N/A`);
+  Object.keys(caseVitals).forEach(key => {
+    const data = caseVitals[key].slice(0, currentIndex);
+    svgVitals.select(`.line.${key}`).attr("d", lineVitals(data));
+  });
+
+  Object.keys(caseInterventions).forEach(key => {
+    const data = caseInterventions[key].slice(0, currentIndex);
+    svgInterventions.select(`.line.${key}`).attr("d", lineInterventions(data));
   });
 }
 
-// FIXED: match button IDs to HTML
 d3.select("#play").on("click", () => {
   if (!playing) {
     playing = true;
@@ -126,22 +140,10 @@ d3.select("#speed").on("click", () => {
   }
 });
 
-function updateChart() {
-  const caseData = vitalsData[currentCase];
-  const vitals = Object.keys(caseData);
-
-  vitals.forEach(key => {
-    const data = caseData[key].slice(0, currentIndex);
-    g.select(`.line.${key}`).attr("d", line(data));
-
-    const last = data[data.length - 1];
-    d3.select(`#vital-${key}`).text(`${key}: ${last ? last.value : "N/A"}`);
-  });
-}
-
 d3.select("#case-select").on("change", function () {
   currentCase = this.value;
-  g.selectAll("*").remove();
+  svgVitals.selectAll("*").remove();
+  svgInterventions.selectAll("*").remove();
   currentIndex = 0;
   init();
 });
